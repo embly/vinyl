bde178e6587668260bdfda4e42cdb07ecb70facb
maxmcd <max.t.mcdonnell@gmail.com>
2019-11-27 09:23:30 +0000

    async requests partial rewrite

--- vinyl-rs-embly/src/lib.rs
+++ vinyl-rs-embly/src/lib.rs
@@ -61,123 +61,21 @@
         clippy::use_self
     )
 )]
-
+use embly::spawn_function;
+use embly::Conn;
+pub use embly::Waitable;
+use failure::{err_msg, Error};
+use protobuf::{parse_from_bytes, Message};
+use std::future::Future;
+use std::io::Read;
+use std::io::Write;
+use std::pin::Pin;
+use std::str;
+use std::task::{Context, Poll};
+use vinyl_core::proto::transport::{Request, Response as ProtoResponse};
 pub use vinyl_core::query;
 pub use vinyl_core::DefaultValue;
 pub use vinyl_core::ToValue;
-pub use embly::Waitable;
-use embly::{spawn_function, Conn};
-use failure::{err_msg, Error};
-use protobuf::{parse_from_bytes, Message};
-use std::io::Read;
-use std::io::Write;
-use std::marker::PhantomData;
-use std::str;
-use vinyl_core::proto::transport::{Request, Response};
-
-struct ProtoResponseWaitable {
-    conn: Conn,
-}
-
-impl Waitable for ProtoResponseWaitable {
-    type Output = Result<Response, Error>;
-
-    fn id(&self) -> i32 {
-        self.conn.id()
-    }
-    fn fetch_result(&mut self) -> Result<Response, Error> {
-        let mut buffer = Vec::new();
-        self.conn.read_to_end(&mut buffer)?;
-        let mut response: Response = parse_from_bytes(&buffer)?;
-        // response
-        let err = response.take_error();
-        if !err.is_empty() {
-            Err(err_msg(err))
-        } else {
-            Ok(response)
-        }
-    }
-}
-
-/// a future that returns records
-pub struct RecordsWaitable<T> {
-    response: ProtoResponseWaitable,
-    phantom: PhantomData<T>,
-}
-
-impl<T: Message> Waitable for RecordsWaitable<T> {
-    type Output = Result<Vec<T>, Error>;
-
-    fn id(&self) -> i32 {
-        self.response.id()
-    }
-
-    fn fetch_result(&mut self) -> Result<Vec<T>, Error> {
-        let resp = self.response.wait()?;
-        let mut v: Vec<T> = Vec::new();
-        for record in resp.get_records().iter() {
-            v.push(parse_from_bytes(record)?);
-        }
-        Ok(v)
-    }
-}
-
-/// returns an empty value on success
-pub struct EmptyRecordWaitable {
-    response: ProtoResponseWaitable,
-}
-
-impl Waitable for EmptyRecordWaitable {
-    type Output = Result<(), Error>;
-
-    fn id(&self) -> i32 {
-        self.response.id()
-    }
-
-    fn fetch_result(&mut self) -> Result<(), Error> {
-        self.response.wait()?;
-        Ok(())
-    }
-}
-
-/// record future
-pub struct RecordWaitable<T> {
-    response: ProtoResponseWaitable,
-    phantom: PhantomData<T>,
-}
-
-impl<T: Message> Waitable for RecordWaitable<T> {
-    type Output = Result<T, Error>;
-
-    fn id(&self) -> i32 {
-        self.response.id()
-    }
-    fn fetch_result(&mut self) -> Result<T, Error> {
-        let resp = self.response.wait()?;
-        let record = match resp.get_records().first() {
-            Some(record) => record,
-            None => return Err(err_msg("no record found")),
-        };
-        Ok(parse_from_bytes(record)?)
-    }
-}
-
-/// a future for an empty response
-pub struct ResponseWaitable {
-    response: ProtoResponseWaitable,
-}
-
-impl Waitable for ResponseWaitable {
-    type Output = Result<(), Error>;
-
-    fn id(&self) -> i32 {
-        self.response.id()
-    }
-    fn fetch_result(&mut self) -> Result<(), Error> {
-        self.response.wait()?;
-        Ok(())
-    }
-}
 
 /// the db
 pub struct DB {
@@ -185,11 +83,40 @@ pub struct DB {
     session_token: String,
 }
 
+// struct Response<T> {
+//     conn: Conn,
+//     value: T,
+// }
+
+// impl<Y> Future for Response<Vec<Y>>
+// where
+//     Y: Message,
+// {
+//     type Output = Result<Vec<Y>, Error>;
+//     fn poll(mut self: Pin<&mut Self>, _cx: &mut Context) -> Poll<Self::Output> {
+//         let poll = Pin::new(&mut self.conn).poll(_cx);
+//         if let Poll::Ready(_) = poll {
+//             // let mut buffer = Vec::new();
+//             // self.conn.read_to_end(&mut buffer).unwrap();
+//             // let mut response: ProtoResponse = parse_from_bytes(&buffer)?;
+//             // let err = response.take_error();
+//             // let mut v: Vec<Y> = Vec::new();
+//             // for record in response.get_records().iter() {
+//             //     v.push(parse_from_bytes(record)?);
+//             // }
+//             let mut v: Vec<Y> = Vec::new();
+//             Poll::Ready(Ok(v))
+//         } else {
+//             Poll::Pending
+//         }
+//     }
+// }
+
 impl DB {
     /// make a new one
-    pub fn new(name: &str) -> Result<Self, Error> {
+    pub async fn new(name: &str) -> Result<Self, Error> {
         let mut conn = spawn_function(&format!("embly/vinyl/{}/connect", name))?;
-        conn.wait()?;
+        conn.await?;
         let mut buf = Vec::new();
         conn.read_to_end(&mut buf)?;
         Ok(Self {
@@ -199,43 +126,116 @@ impl DB {
     }
 
     /// return records that match the provided query
-    pub fn execute_query<T: Message>(&self, q: query::Query) -> Result<RecordsWaitable<T>, Error> {
+    pub async fn execute_query<T: Message>(&self, q: query::Query) -> Result<Vec<T>, Error> {
         let req = vinyl_core::execute_query_request::<T>(q);
-        let response = self.send_request(req)?;
-        Ok(RecordsWaitable {
-            response,
-            phantom: PhantomData,
-        })
+        let conn = self.send_request(req)?;
+        let response = self.await_response(conn).await?;
+        let mut v: Vec<T> = Vec::new();
+        for record in response.get_records().iter() {
+            v.push(parse_from_bytes(record)?);
+        }
+        Ok(v)
     }
-
-    /// asdf
-    pub fn insert<T: Message>(&self, msg: T) -> Result<EmptyRecordWaitable, Error> {
+    fn insert_sync<T: Message>(&self, msg: T) -> Result<Conn, Error> {
         let req = vinyl_core::insert_request::<T>(msg)?;
-        let response = self.send_request(req)?;
-        Ok(EmptyRecordWaitable { response })
+        let conn = self.send_request(req)?;
+        Ok(conn)
     }
+    /// asdfasdf
+    pub fn insert<T: Message>(&self, msg: T) -> impl Future<Output = Result<(), Error>> {
+        let result = self.insert_sync(msg);
+        async {
+            match result {
+                Ok(conn) => {
+                    let _response = await_response(conn.clone()).await?;
+                    Ok(())
+                }
+                Err(err) => {
+                    return Err(err);
+                }
+            }
+        }
+    }
+    /// asdf
+    pub fn insert_again<T: Message>(&self, msg: T) -> impl Future<Output = Result<(), Error>> {
+        let req = vinyl_core::insert_request::<T>(msg).unwrap();
+        let conn = self.send_request(req).unwrap();
+        async move {
+            let _response = await_response(conn).await?;
+            Ok(())
+        }
+        // TODO: return any errors
+    }
+    // pub fn insert_again<T: Message>(
+    //     &self,
+    //     msg: T,
+    // ) -> Result<impl Future<Output = Result<(), Error>>, Error> {
+    //     let req = vinyl_core::insert_request::<T>(msg)?;
+    //     let conn = self.send_request(req)?;
+    //     Ok(async move {
+    //         let _response = self.await_response(conn).await?;
+    //         Ok(())
+    //     })
+    //     // TODO: return any errors
+    // }
+    /// asdf
+    // pub async fn insert<T: Message>(&self, msg: T) -> Result<(), Error> {
+    //     let req = vinyl_core::insert_request::<T>(msg)?;
+    //     let conn = self.send_request(req)?;
+    //     let _response = self.await_response(conn).await?;
+    //     Ok(())
+    //     // TODO: return any errors
+    // }
 
     /// asdf
-    pub fn load_record<T: Message, K: ToValue>(&self, pk: K) -> Result<RecordWaitable<T>, Error> {
+    pub async fn load_record<T: Message, K: ToValue>(&self, pk: K) -> Result<T, Error> {
         let req = vinyl_core::load_record::<T, K>(pk);
-        let response = self.send_request(req)?;
-        Ok(RecordWaitable {
-            response,
-            phantom: PhantomData,
-        })
+        let conn = self.send_request(req)?;
+        let resp = self.await_response(conn).await?;
+        let record = match resp.get_records().first() {
+            Some(record) => record,
+            None => return Err(err_msg("no record found")),
+        };
+        Ok(parse_from_bytes(record)?)
     }
 
     /// delete records that match the provided query
-    pub fn delete_record<T: Message, K: ToValue>(&self, pk: K) -> Result<ResponseWaitable, Error> {
+    pub async fn delete_record<T: Message, K: ToValue>(&self, pk: K) -> Result<(), Error> {
         let req = vinyl_core::delete_record::<T, K>(pk);
-        let resp = self.send_request(req)?;
-        Ok(ResponseWaitable { response: resp })
+        let conn = self.send_request(req)?;
+        self.await_response(conn).await?;
+        Ok(())
     }
 
-    fn send_request(&self, mut req: Request) -> Result<ProtoResponseWaitable, Error> {
+    fn send_request(&self, mut req: Request) -> Result<Conn, Error> {
         let mut conn = spawn_function(&format!("embly/vinyl/{}/request", self.name))?;
         req.set_token(self.session_token.clone());
         conn.write_all(&req.write_to_bytes()?)?;
-        Ok(ProtoResponseWaitable { conn: conn })
+        Ok(conn)
+    }
+    async fn await_response(&self, mut conn: Conn) -> Result<ProtoResponse, Error> {
+        conn.await?;
+        let mut buffer = Vec::new();
+        conn.read_to_end(&mut buffer)?;
+        let mut response: ProtoResponse = parse_from_bytes(&buffer)?;
+        let err = response.take_error();
+        if !err.is_empty() {
+            Err(err_msg(err))
+        } else {
+            Ok(response)
+        }
+    }
+}
+
+async fn await_response(mut conn: Conn) -> Result<ProtoResponse, Error> {
+    conn.await?;
+    let mut buffer = Vec::new();
+    conn.read_to_end(&mut buffer)?;
+    let mut response: ProtoResponse = parse_from_bytes(&buffer)?;
+    let err = response.take_error();
+    if !err.is_empty() {
+        Err(err_msg(err))
+    } else {
+        Ok(response)
     }
 }
